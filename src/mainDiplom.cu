05cc3f5f51be65c7f059f9e56970d8e1ccf6571a
plotva <grigorii@shveps@gmail.com>
2023-05-07 19:33:43 +0300

    rework

--- src/mainDiplom.cu
+++ src/mainDiplom.cu
@@ -23,27 +23,12 @@ using namespace std;
 
 int main () {
     double solving_time, allTimeSolving = (double) getTickCount();
-    double costTime, pathTime, disparityTime;
-
 
     Mat leftImage = cv::imread("./src/images/leftImage1.png",cv::IMREAD_GRAYSCALE);
     Mat rightImage = cv::imread("./src/images/rightImage1.png",cv::IMREAD_GRAYSCALE);
 
     size_t cols = leftImage.cols, rows = leftImage.rows;
-
-
-    int *pix_cost = (int *) calloc(rows * cols * D_LVL, sizeof(int));
-    int *sum_cost = (int *) calloc(rows * cols * D_LVL, sizeof(int));
-    
-    if (!pix_cost || !sum_cost) {
-        printf("mem failure, exiting A \n");
-        exit(EXIT_FAILURE);
-    }
-
-    cv::Mat *census_l = new cv::Mat(rows, cols, CV_8UC1);
-    cv::Mat *census_r = new cv::Mat(rows, cols, CV_8UC1);
     cv::Mat disparityMap, *dispImg = new cv::Mat(rows, cols, CV_8UC1);
-
 
     cout << " Start timing"<< endl;
     solving_time = (double) getTickCount();
@@ -86,55 +71,71 @@ int main () {
     // uint **CSCTLeftImage = toCSCT(arrayLeftImage, rows, cols);
     // uint **CSCTRightImage = toCSCT(arrayRightImage, rows, cols);
 
-  // cout << "1. Census Transform" << endl;
-  census_transform(leftImage, *census_l, rows, cols);
-  census_transform(rightImage, *census_r, rows, cols);
 
-  // 2. Calculate Pixel Cost.
-  cout << "1. Calculate Pixel Cost." << endl;
-  costTime = (double) getTickCount();
+    double costTime, pathTime, disparityTime;
+    int *pix_cost = (int *) calloc(rows * cols * D_LVL, sizeof(int));
+    int *sum_cost = (int *) calloc(rows * cols * D_LVL, sizeof(int));
+    
+    if (!pix_cost || !sum_cost) {
+        printf("mem failure, exiting A \n");
+        exit(EXIT_FAILURE);
+    }
 
-  calcCost_CUDA(*census_l, *census_r, pix_cost, rows, cols);
-  costTime = ((double)getTickCount() - costTime)/getTickFrequency();
+    cv::Mat *census_l = new cv::Mat(rows, cols, CV_8UC1);
+    cv::Mat *census_r = new cv::Mat(rows, cols, CV_8UC1);
+
+    // cout << "1. Census Transform" << endl;
+    census_transform(leftImage, *census_l, rows, cols);
+    census_transform(rightImage, *census_r, rows, cols);
+
+    // 2. Calculate Pixel Cost.
+    cout << "1. Calculate Pixel Cost." << endl;
+    costTime = (double) getTickCount();
+
+    calcCost_CUDA(*census_l, *census_r, pix_cost, rows, cols);
+    costTime = ((double)getTickCount() - costTime)/getTickFrequency();
 
 
-  // 3. Aggregate Cost
-  cout << "2. Aggregate Cost" << endl;
-  pathTime = (double) getTickCount();
-  optimized_agregateCostCUDA(pix_cost, sum_cost, rows, cols);   // 20ms
-  pathTime = ((double)getTickCount() - pathTime)/getTickFrequency();
+    // 3. Aggregate Cost
+    cout << "2. Aggregate Cost" << endl;
+    pathTime = (double) getTickCount();
+    optimized_agregateCostCUDA(pix_cost, sum_cost, rows, cols);   // 20ms
+    pathTime = ((double)getTickCount() - pathTime)/getTickFrequency();
 
 
-  // 4. Create Disparity Image.
-  cout << "3. Create Disparity Image." << endl;
-  disparityTime = (double) getTickCount();
-  calc_disparity(sum_cost, *dispImg, rows, cols);
-  disparityTime = ((double)getTickCount() - disparityTime)/getTickFrequency();
+    // 4. Create Disparity Image.
+    cout << "3. Create Disparity Image." << endl;
+    disparityTime = (double) getTickCount();
+    calc_disparity(sum_cost, *dispImg, rows, cols);
+    disparityTime = ((double)getTickCount() - disparityTime)/getTickFrequency();
 
-  // Visualize Disparity Image.
-  disparityMap = *dispImg;
-  disparityMap.convertTo(disparityMap, CV_8U, 256.0/D_LVL);
-  applyColorMap(disparityMap, disparityMap, COLORMAP_JET);
-  imshow("leftImage", disparityMap);
 
-  // END TEST GRAYSCALE
 
-  solving_time = ((double)getTickCount() - solving_time)/getTickFrequency();
-  allTimeSolving = ((double)getTickCount() - allTimeSolving)/getTickFrequency();
-  cout.precision(3);
-  cout<<"Cost algorithm time: "<< costTime <<"s"<<endl;  // 120ms
-  cout<<"Path algorithm time: "<< pathTime <<"s"<<endl;  // 48ms
-  cout<<"Disparity algorithm time: "<< disparityTime <<"s"<<endl;  // 36ms
-  cout<<"Process time: "<<solving_time<<"s"<<endl;     // 230ms
-  cout<<"All run time: "<<allTimeSolving<<"s"<<endl;   // 233ms
-  std::cout << "OK"<< std::endl;
-  while(1)
-  {
-      short key = (short)waitKey();
-      if( key == 27 || key == 'q' || key == 'Q') // 'ESC'
-          break;
-  }
-  
-  return 0;
+
+    // Visualize Disparity Image.
+    disparityMap = *dispImg;
+    disparityMap.convertTo(disparityMap, CV_8U, 256.0/D_LVL);
+    applyColorMap(disparityMap, disparityMap, COLORMAP_JET);
+    imshow("leftImage", disparityMap);
+
+    // END TEST GRAYSCALE
+
+    solving_time = ((double)getTickCount() - solving_time)/getTickFrequency();
+    allTimeSolving = ((double)getTickCount() - allTimeSolving)/getTickFrequency();
+    cout.precision(3);
+    cout<<"Cost algorithm time: "<< costTime <<"s"<<endl;  // 120ms
+    cout<<"Path algorithm time: "<< pathTime <<"s"<<endl;  // 48ms
+    cout<<"Disparity algorithm time: "<< disparityTime <<"s"<<endl;  // 36ms
+    cout<<"Process time: "<<solving_time<<"s"<<endl;     // 230ms
+    cout<<"All run time: "<<allTimeSolving<<"s"<<endl;   // 233ms
+    std::cout << "OK"<< std::endl;
+    while(1)
+    {
+        short key = (short)waitKey();
+        if( key == 27 || key == 'q' || key == 'Q') // 'ESC'
+            break;
+    }
+
+    return 0;
 }
 
